76956b8b8117241dae0e73ba1f00b9c4a0a3b1a3
Ravi <rkumar@g.hmc.edu>
2014-04-28 19:04:15 -0700

    added player

--- match_manager.erl
+++ match_manager.erl
@@ -2,6 +2,8 @@
 
 -export ([init/4]).
 
+-import(utils).
+
 %% TODO: Game manager is just a function of match_manager. Separate files only if its huge
 
 
@@ -9,5 +11,15 @@
 %% TMID - tournament manager id
 init(PLayerOne, PlayerTwo, K, TMID) ->
   %% run upto K games, tally results and send them back to tournament
-  what.
+  match_done.
 
+
+
+match(PlayerOne, PlayerTwo, K, TMID, {P1Score, P2Score}) when P1Score > K/2 ->
+	TMID ! {match_done, PlayerOne};
+
+match(PlayerOne, PlayerTwo, K, TMID, {P1Score, P2Score}) when P2Score > K/2 ->
+	TMID ! {match_done, PlayerTwo};
+
+match(PlayerOne, PlayerTwo, K, TMID, {P1Score, P2Score}) ->
+	
--- yahtzee_player1.erl
+++ yahtzee_player1.erl
@@ -4,6 +4,11 @@
 -module(yahtzee_player1).
 -export(main/1).
 
+-import(utils).
+
+-define(LOGINPID, 1).
+-define(LOGINTICKET, 2).
+
 main([NetName, Username, Password | YMNames]) ->
 	%Boring erlang net stuff
 	_ = os:cmd("epmd -daemon"),
@@ -11,20 +16,83 @@ main([NetName, Username, Password | YMNames]) ->
     %register(philosopher, self()),
 
 	%Register with all managers
+	Tickets = send_register_messages(YMNames, Username, Password, []),
 
-	%Accumulate responses? and pull data for that
-	%Start the listen guy that will just respond to messages and loop
-	%hi
+	%Now just chill and listen
+	listen(Username, Tickets),
+	plog("Uh oh! We shouldn't really be here.", Username),
 	hurray.
 
-send_register_messages([]) ->
-	sent.
+send_register_messages([], _Username, _Password, Tickets) ->
+	Tickets;
 
-send_register_messages([Name | YMNames], Username, Password) ->
+send_register_messages([Name | YMNames], Username, Password, Tickets) ->
+	%Send the manager a login message
+	plog("Logging in with manager ~p", [Name], Username),
 	Pid = whereis_name(Name),
 	Pid ! {login, self(), Username, {Username, Password}},
-	send_register_messages(YMNames),
-	almost sent.
 
-listen() ->
-	listening.
+	%receive the ticket from the login manager
+	%When we get it, go on to the next guy
+	receive
+		{logged_in, LogoutPID, Username, LoginTicket} ->
+			plog("Got login ticket from manager ~p", [Name], Username),
+			send_register_messages(YMNames, Username, Password, [{LogoutPID, LoginTicket} | Tickets])
+	end.
+
+%Currently there is no system for logging out, so we don't actually need tickets
+%We also don't keep track of what tournaments we're in
+listen(Username, Tickets) ->
+	receive
+		%Start tournament message
+		{start_tournament, ReplyPID, Username, TID} ->
+			plog("Got message to start tournament with ID ~p", [TID], Username),
+			%Find the ticket we logged in with to this YM and reply with that ticket
+			{ReplyPID, LoginTicket} = lists:keyfind(ReplyPID, ?LOGINPID, Tickets),
+			plog("Replying with acceptance.", Username),
+			ReplyPID ! {accept_tournament, self(), Username, {TID, LoginTicket}};
+
+		%End tournament message
+		{end_tournament, _ReplyPID, _Username, TID} -> 
+			plog("Received end tournament message with ID ~p", [TID], Username),
+			we_dont_care;
+
+		%Play request message! What we actually care about
+		{play_request, ReplyPID, Username, GameState} ->
+			plog("Received player request with game state: ~n  ~p", [GameState], Username),
+			Action = computeAction(GameState),
+			ReplyPID ! {play_action, self(), Username, Action}
+	end,
+	listen(Username, Tickets).
+
+
+%Current action computation: Assign it to whatever first slot is free, no rerolls
+computeAction({Ref, Tid, Gid, RollNumber, _Dice, Scorecard, _OppScorecard}) ->
+	%Find first empty score
+	EmptyScores = findEmptyScores(Scorecard)
+	case EmptyScores of
+		[] -> plog("Someone sent us a full scorecard! Morons...");
+		_ ->
+			{Ref, Tid, Gid, RollNumber, [true, true, true, true, true], hd(EmptyScores)}
+	end.
+	
+
+%Compute action related helpers
+findEmptyScores(Scorecard) -> findEmptyScores(Scorecard, [], 1).
+findEmptyScores([], Empty, _Index) -> lists:reverse(Empty);
+findEmptyScores([-1 | Rest], EmptySoFar, Index) ->
+	findEmptyScores(Rest, [Index | EmptySoFar], Index + 1);
+findEmptyScores([_ | Rest], EmptySoFar, Index) ->
+	findEmptyScores(Rest, EmptySoFar, Index).
+
+	
+	
+
+
+%Player log message. Essentially just prints the name as well
+plog(Message, Name) ->
+	utils:timestamp(),
+ 	io:format("~p: ~s~n", [Name, Message]).
+plog(Message, Format, Name) ->
+	S = io_lib:format(Message, Format),
+	log(S, Name).
